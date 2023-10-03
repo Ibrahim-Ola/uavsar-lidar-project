35c2b9f682f7ec69ad60270f7c0aaae660c30214
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2023-10-03 14:04:16 -0600

    adding features script

---
+++ utils/features.py
@@ -0,0 +1,21 @@
+
+BASELINE_FEATURES=['elevation']
+
+UAVSAR_FEATURES=[
+    'coherence',
+    'amplitude',
+    'wrapped_phase',
+    'bare_earth_dem',
+    'incidence_angle',
+    'unwrapped_phase'
+]
+
+UAVSAR_AND_VEGETATION_HEIGHT_FEATURES=[
+    'coherence',
+    'amplitude',
+    'vegetation_ht',
+    'wrapped_phase',
+    'bare_earth_dem',
+    'incidence_angle',
+    'unwrapped_phase'
+]
