ba4f9f9e8f85b0ebee86993250362f5682e4dcfa
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-01-04 00:34:48 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,79 @@
 
 from setuptools import setup, find_packages
 
-DISTNAME = "uavsar-lidar-project"
+PROJNAME = "uavsar-lidar-project"
 DESCRIPTION = "A project that uses NASA JPL UAVSAR data to predict total snow depth."
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 MAINTAINER = "Ibrahim Olalekan Alabi"
 MAINTAINER_EMAIL = "ibrahimolalekana@u.boisestate.edu"
-URL = "https://github.com/Ibrahim-Ola"
+URL = "https://github.com/Ibrahim-Ola/uavsar-lidar-project.git"
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/Ibrahim-Ola/uavsar-lidar-project/issues",
+    "Documentation": "https://github.com/Ibrahim-Ola/uavsar-lidar-project/blob/main/README.md",
+    "Source Code": "https://github.com/Ibrahim-Ola/uavsar-lidar-project",
+}
+VERSION = "0.0.1"
+LICENSE = "MIT"
+PYTHON_REQUIRES = ">=3.9"
+
+
+def setup_package():
+    metadata = dict(
+        name=PROJNAME,
+        version=VERSION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+        url=URL,
+        project_urls=PROJECT_URLS,
+        license=LICENSE,
+        python_requires=PYTHON_REQUIRES,
+        packages=find_packages(),
+        install_requires=[
+            "numpy",
+            "pandas",
+            "matplotlib",
+            "scipy",
+            "scikit-learn",
+            "gdal",
+            "rasterio",
+            "pyproj",
+            "geopandas",
+            "shapely",
+            "descartes",
+            "seaborn",
+            "tqdm",
+            "rich",
+        ],
+        classifiers=[
+            "Development Status :: Mature",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering :: Hydrology",
+        ],
+        keywords=[
+            "UAVSAR",
+            "LIDAR",
+            "NISAR",
+            "Snow",
+            "Snow Depth",
+            "Snow Water Equivalent",
+            "Machine Learning",
+            "Remote Sensing",
+        ],
+    )
+
+    setup(**metadata)
+
+if __name__ == "__main__":
+    setup_package()
+    print("Setup Complete.")
