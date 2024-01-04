ffc4f88be0715f05e30252772855e3ee7aad4b79
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-01-03 21:19:33 -0700

    initiated setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+
+from setuptools import setup, find_packages
+
+DISTNAME = "uavsar-lidar-project"
+DESCRIPTION = "A project that uses NASA JPL UAVSAR data to predict total snow depth."
+MAINTAINER = "Ibrahim Olalekan Alabi"
+MAINTAINER_EMAIL = "ibrahimolalekana@u.boisestate.edu"
+URL = "https://github.com/Ibrahim-Ola"
